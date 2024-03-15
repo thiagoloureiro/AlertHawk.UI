43be1753ea4bce42376bcc6a7bbd056373efe520
Altughan Ozengi <altughan.ozengi@gmail.com>
2024-03-15 10:50:47 +0100

    Added selected monitor details

--- src/pages/Dashboard/Items/SelectedMonitorDetails.tsx
+++ src/pages/Dashboard/Items/SelectedMonitorDetails.tsx
@@ -41,45 +41,44 @@ const SelectedMonitorDetails: FC<ISelectedMonitorDetailsProps> = ({
   };
 
   return (
-    <Card>
-      <CardContent>
-        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-          <Typography variant="h5" mb={2}>
-            {selectedMonitorGroup?.name || selectedMonitorItem?.name}
-          </Typography>
-          <ButtonGroup
-            variant="contained"
-            size="large"
-            color="secondary"
-            disableElevation
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 4 }}>
+      <Typography variant="h5" px={2}>
+        {selectedMonitorGroup?.name || selectedMonitorItem?.name}
+      </Typography>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 2, px: 2 }}>
+        <ButtonGroup
+          variant="contained"
+          size="large"
+          color="secondary"
+          disableElevation
+        >
+          <Button
+            aria-label="pause/resume"
+            startIcon={isPaused ? <PlayArrowIcon /> : <PauseIcon />}
+            onClick={handleResumePauseBtn}
           >
-            <Button
-              aria-label="pause/resume"
-              startIcon={isPaused ? <PlayArrowIcon /> : <PauseIcon />}
-              onClick={handleResumePauseBtn}
-            >
-              <Box sx={{ display: "flex", alignItems: "center" }}>
-                {isPaused ? t("dashboard.resume") : t("dashboard.pause")}
-              </Box>
-            </Button>
-            <Button
-              aria-label="edit"
-              startIcon={<EditNoteIcon />}
-              onClick={handleEditBtn}
-            >
-              {t("dashboard.edit")}
-            </Button>
-            <Button
-              aria-label="delete"
-              startIcon={
-                <DeleteIcon sx={{ color: isDarkMode ? "inherit" : "#fff" }} />
-              }
-              color="error"
-            >
-              {t("dashboard.delete")}
-            </Button>
-          </ButtonGroup>
-          {/* <Typography
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              {isPaused ? t("dashboard.resume") : t("dashboard.pause")}
+            </Box>
+          </Button>
+          <Button
+            aria-label="edit"
+            startIcon={<EditNoteIcon />}
+            onClick={handleEditBtn}
+          >
+            {t("dashboard.edit")}
+          </Button>
+          <Button
+            aria-label="delete"
+            startIcon={
+              <DeleteIcon sx={{ color: isDarkMode ? "inherit" : "#fff" }} />
+            }
+            color="error"
+          >
+            {t("dashboard.delete")}
+          </Button>
+        </ButtonGroup>
+        {/* <Typography
                   component="div"
                   variant="h5"
                   sx={{
@@ -93,9 +92,149 @@ const SelectedMonitorDetails: FC<ISelectedMonitorDetailsProps> = ({
                 >
                   Up
                 </Typography> */}
-        </Box>
-      </CardContent>
-    </Card>
+      </Box>
+      {selectedMonitorItem !== null && (
+        <>
+          <Card>
+            <CardContent>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Typography variant="h6">Uptime</Typography>
+                  <Typography variant="body2">(24 hours)</Typography>
+                  <Typography variant="subtitle1">
+                    {selectedMonitorItem.monitorStatusDashboard.uptime24Hrs ??
+                      "N/A"}
+                    {selectedMonitorItem.monitorStatusDashboard.uptime24Hrs &&
+                      "%"}
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Typography variant="h6">Uptime</Typography>
+                  <Typography variant="body2">(7 days)</Typography>
+                  <Typography variant="subtitle1">
+                    {selectedMonitorItem.monitorStatusDashboard.uptime7Days ??
+                      "N/A"}
+                    {selectedMonitorItem.monitorStatusDashboard.uptime7Days &&
+                      "%"}
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Typography variant="h6">Uptime</Typography>
+                  <Typography variant="body2">(30 days)</Typography>
+                  <Typography variant="subtitle1">
+                    {selectedMonitorItem.monitorStatusDashboard.uptime30Days ??
+                      "N/A"}
+                    {selectedMonitorItem.monitorStatusDashboard.uptime30Days &&
+                      "%"}
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Typography variant="h6">Uptime</Typography>
+                  <Typography variant="body2">(3 months)</Typography>
+                  <Typography variant="subtitle1">
+                    {selectedMonitorItem.monitorStatusDashboard.uptime3Months ??
+                      "N/A"}
+                    {selectedMonitorItem.monitorStatusDashboard.uptime3Months &&
+                      "%"}
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Typography variant="h6">Uptime</Typography>
+                  <Typography variant="body2">(6 months)</Typography>
+                  <Typography variant="subtitle1">
+                    {selectedMonitorItem.monitorStatusDashboard.uptime6Months ??
+                      "N/A"}
+                    {selectedMonitorItem.monitorStatusDashboard.uptime6Months &&
+                      "%"}
+                  </Typography>
+                </Box>
+              </Box>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardContent>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Typography variant="h6">Response</Typography>
+                  <Typography variant="subtitle1">
+                    {selectedMonitorItem.monitorStatusDashboard.responseTime +
+                      " ms"}
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Typography variant="h6">Certificate Expiration</Typography>
+                  <Typography variant="subtitle1">
+                    {selectedMonitorItem.monitorStatusDashboard.certExpDays +
+                      " days"}
+                  </Typography>
+                </Box>
+              </Box>
+            </CardContent>
+          </Card>
+        </>
+      )}
+    </Box>
   );
 };
 
