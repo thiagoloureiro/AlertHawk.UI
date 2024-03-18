3afbc8dc8cf4b3deca81b7cc93c28d1d5e5cc107
Altughan Ozengi <altughan.ozengi@gmail.com>
2024-03-18 16:43:45 +0100

    Restricted access to user management

---
+++ src/components/NotFoundContent/NotFoundContent.tsx
@@ -0,0 +1,50 @@
+import { Button, Stack, Typography } from "@mui/material";
+import { FC } from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import { useStoreState } from "../../hooks";
+
+const NotFoundContent: FC<{}> = () => {
+  const navigate = useNavigate();
+  const { t } = useTranslation("global");
+  const { isSmallScreen } = useStoreState((state) => state.app);
+
+  const handleRedirect = () => {
+    navigate("/", { replace: true });
+  };
+
+  return (
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={3}
+      sx={{ minHeight: "calc(100vh - 170px)" }}
+    >
+      {isSmallScreen ? (
+        <Typography
+          variant="h6"
+          gutterBottom
+          mb={2}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <span style={{ fontSize: 26, fontWeight: 500 }}>404</span>
+          <span>{t("pageNotFound")}</span>
+        </Typography>
+      ) : (
+        <Typography variant="h5" gutterBottom mb={2}>
+          404 | {t("pageNotFound")}
+        </Typography>
+      )}
+      <Button variant="text" size="large" onClick={handleRedirect}>
+        Go Back to Dashboard
+      </Button>
+    </Stack>
+  );
+};
+
+export default NotFoundContent;
--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,107 +1,129 @@
-import { Box, Card, CardContent, Grid, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import { FC, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useStoreActions, useStoreState } from "../../hooks";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import NotFoundContent from "../../components/NotFoundContent/NotFoundContent";
 
-interface IUsersProps { }
+interface IUsersProps {}
 
 interface IHeaderCell {
-    id: string;
-    label: string;
-    width?: string;
-    sortable: boolean;
-    align?: "left" | "center" | "right" | "justify" | "inherit"
+  id: string;
+  label: string;
+  width?: string;
+  sortable: boolean;
+  align?: "left" | "center" | "right" | "justify" | "inherit";
 }
 
 const Users: FC<IUsersProps> = () => {
-    const { t } = useTranslation("global");
-    const { users } = useStoreState((state) => state.user);
-    const { thunkGetAllUsers } = useStoreActions((state) => state.user);
+  const { t } = useTranslation("global");
+  const { user, users } = useStoreState((state) => state.user);
+  const { thunkGetAllUsers, setUsers } = useStoreActions((state) => state.user);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            await thunkGetAllUsers();
-        };
-        if (users.length == 0) {
-            fetchData();
-        }
-    }, [users]);
+  useEffect(() => {
+    const fetchData = async () => {
+      await thunkGetAllUsers();
+    };
+    if (user?.isAdmin) {
+      fetchData();
+    } else {
+      setUsers([]);
+    }
+  }, []);
 
+  const headerCells: readonly IHeaderCell[] = [
+    {
+      id: "userName",
+      label: t("users.userName"),
+      sortable: false,
+    },
+    {
+      id: "Email",
+      label: t("users.email"),
+      sortable: true,
+    },
+    {
+      id: "isAdmin",
+      label: t("users.isAdmin"),
+      width: "220px",
+      sortable: true,
+    },
+    {
+      id: "Actions",
+      label: t("users.actions"),
+      sortable: true,
+    },
+  ];
 
-    const headerCells: readonly IHeaderCell[] = [
-        {
-            id: 'userName',
-            label: t("users.userName"),
-            sortable: false
-        },
-        {
-            id: 'Email',
-            label: t("users.email"),
-            sortable: true
-        },
-        {
-            id: 'isAdmin',
-            label: t("users.isAdmin"),
-            width: '220px',
-            sortable: true
-        },
-        {
-            id: 'Actions',
-            label: t("users.actions"),
-            sortable: true
-        }
-    ];
-
-    return (
+  return (
+    <>
+      {!user?.isAdmin ? (
+        <NotFoundContent />
+      ) : (
         <>
-            <HelmetProvider>
-                <Helmet>
-                    <title>AlertHawk | {t("users.text")}</title>
-                </Helmet>
-            </HelmetProvider>
-            <Grid container spacing={4}>
-                <Grid item xs={12}>
-                    <Card >
-                        <CardContent>
-                            <Box
-                                sx={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                    justifyContent: "center",
-                                    cursor: "pointer",
-                                }}
-                            >
-                                <Table >
-                                    <TableHead>
-                                        <TableRow>
-                                            {
-                                                headerCells.map((headerCell: IHeaderCell) => (
-                                                    <TableCell>{headerCell.label} </TableCell>
-                                                ))
-                                            }
-                                        </TableRow>
-                                    </TableHead>
+          <HelmetProvider>
+            <Helmet>
+              <title>AlertHawk | {t("users.text")}</title>
+            </Helmet>
+          </HelmetProvider>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <Card>
+                <CardContent>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      cursor: "pointer",
+                    }}
+                  >
+                    <Table>
+                      <TableHead>
+                        <TableRow>
+                          {headerCells.map((headerCell: IHeaderCell, index) => (
+                            <TableCell key={index}>
+                              {headerCell.label}{" "}
+                            </TableCell>
+                          ))}
+                        </TableRow>
+                      </TableHead>
 
-                                    <TableBody>
-                                        {users.length > 0 &&
-                                            users.map((_user, i) => (
-                                                <TableRow key={i}>
-                                                    <TableCell>{_user.username}</TableCell>
-                                                    <TableCell>{_user.email}</TableCell>
-                                                    <TableCell>{_user.isAdmin ? t("users.true") : t("users.false")}</TableCell>
-                                                    <TableCell>...</TableCell>
-                                                </TableRow>
-                                            ))}
-                                    </TableBody>
-                                </Table>
-                            </Box>
-                        </CardContent>
-                    </Card>
-                </Grid>
+                      <TableBody>
+                        {users.length > 0 &&
+                          users.map((_user, i) => (
+                            <TableRow key={i}>
+                              <TableCell>{_user.username}</TableCell>
+                              <TableCell>{_user.email}</TableCell>
+                              <TableCell>
+                                {_user.isAdmin
+                                  ? t("users.true")
+                                  : t("users.false")}
+                              </TableCell>
+                              <TableCell>...</TableCell>
+                            </TableRow>
+                          ))}
+                      </TableBody>
+                    </Table>
+                  </Box>
+                </CardContent>
+              </Card>
             </Grid>
+          </Grid>
         </>
-    );
+      )}
+    </>
+  );
 };
 
 export default Users;
