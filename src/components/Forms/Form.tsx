7fa4644566cda7f843ad8dfbcd2a7df3f30b2a26
Thiago Loureiro <thiago.loureiro@pl.abb.com>
2024-05-14 10:24:31 +0200

    fix form login

--- src/components/Forms/Form.tsx
+++ src/components/Forms/Form.tsx
@@ -1,14 +1,10 @@
-import { FC, useState, FormEvent, useEffect } from "react";
+import { FC, useState, useEffect } from "react";
 import {
   Container,
   Typography,
-  TextField,
   Button,
-  CircularProgress,
 } from "@mui/material";
-import { Link } from "react-router-dom";
 import MicrosoftLogoPath from "./microsoft-windows-logo.svg";
-import getTheme from "../../theme";
 import { loginRequest } from "../../config/authConfig";
 import logging from "../../utils/logging";
 import { useMsal } from "@azure/msal-react";
@@ -18,9 +14,6 @@ interface IFormProps {
 }
 
 const Form: FC<IFormProps> = ({ description }) => {
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [step, setStep] = useState<number>(1);
   const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false);
   // const navigate = useNavigate();
   const { instance } = useMsal();
@@ -35,33 +28,6 @@ const Form: FC<IFormProps> = ({ description }) => {
     return () => clearTimeout(timer);
   }, [isButtonDisabled]);
 
-  const simulateLogin = () => {
-    setIsButtonDisabled(true);
-    setTimeout(() => {
-      setIsButtonDisabled(false);
-      //navigate("/dashboard");
-    }, 3000);
-  };
-
-  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = event.target.value.replace(/\s/g, "");
-    setUsername(newValue);
-  };
-
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = event.target.value.replace(/\s/g, "");
-    setPassword(newValue);
-  };
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    if (step === 1 && username) {
-      setStep(2);
-    } else if (step === 2 && password) {
-      simulateLogin();
-    }
-  };
-
   const handleAzureADLogin = () => {
     instance.loginRedirect(loginRequest).catch((e) => {
       logging.error(e);
@@ -70,7 +36,7 @@ const Form: FC<IFormProps> = ({ description }) => {
 
   return (
     <Container maxWidth="sm">
-      <form onSubmit={handleSubmit}>
+      <form>
         <Typography variant="h4" gutterBottom>
           <span style={{ fontWeight: 500 }}>Alert</span>Hawk
         </Typography>
@@ -82,7 +48,7 @@ const Form: FC<IFormProps> = ({ description }) => {
             <b>{description}</b>
           </Typography>
         )}
-      
+
      
       </form>
       <div
