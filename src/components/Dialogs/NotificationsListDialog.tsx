934e9bc61272c402f798deda403f90c190d5ed0a
Guilherme Lima <guilherme.lima@br.abb.com>
2024-05-29 15:21:32 -0300

    NotificationsListDialog change select to listItem

--- src/components/Dialogs/NotificationsListDialog.tsx
+++ src/components/Dialogs/NotificationsListDialog.tsx
@@ -1,19 +1,18 @@
+import * as React from 'react';
 import {
-  Checkbox,
   Dialog,
   DialogTitle,
   FormControl,
-  InputLabel,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
   ListItemText,
-  MenuItem,
-  OutlinedInput,
-  Select,
-  SelectChangeEvent,
-} from "@mui/material";
-import React, { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
-import NotificationService from "../../services/NotificationService";
-import MonitorService from "../../services/MonitorService";
+  Checkbox,
+} from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import NotificationService from '../../services/NotificationService';
+import MonitorService from '../../services/MonitorService';
 
 interface INotificationsListDialog {
   openDialog: boolean;
@@ -26,122 +25,85 @@ const NotificationsListDialog: React.FC<INotificationsListDialog> = ({
   handleCloseDialog,
   monitorId,
 }) => {
-  const { t } = useTranslation("global");
-  const [allNotificationList, setAllNotificationList] = useState<
-    INotification[]
-  >([]);
-  const [selectedNotifications, setSelectedNotifications] = useState<any[]>([]);
+  const { t } = useTranslation('global');
+  const [allNotificationList, setAllNotificationList] = React.useState<INotification[]>([]);
+  const [selectedNotifications, setSelectedNotifications] = React.useState<number[]>([]);
 
-  useEffect(() => {
+  React.useEffect(() => {
+    if (monitorId === undefined) return;
+
     MonitorService.getMonitorNotification(monitorId).then((res) => {
-      const alreadySelected = res.map((element: any) => {
-        return element.notificationId;
-      });
-      const containsAll = alreadySelected.every((element) =>
-        selectedNotifications.includes(element)
-      );
+      const alreadySelected = res.map((element: any) => element.notificationId);
+      const containsAll = alreadySelected.every((element) => selectedNotifications.includes(element));
       if (!containsAll) {
-        setSelectedNotifications((prevState) => [
-          ...prevState,
-          ...alreadySelected,
-        ]);
+        setSelectedNotifications((prevState) => [...prevState, ...alreadySelected]);
       }
     });
 
     const getAllNotification = async () => {
       NotificationService.getAll().then((res) => {
-        const sortedRes = res.sort((a, b) => {
-          if (a.name < b.name) {
-            return -1;
-          }
-          if (a.name > b.name) {
-            return 1;
-          }
-          return 0;
-        });
+        const sortedRes = res.sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0));
         setAllNotificationList(sortedRes);
       });
     };
 
-
     getAllNotification();
   }, [monitorId]);
 
-  const handleChange = (
-    event: SelectChangeEvent<number[]>
-  ) => {
+  const handleToggle = (value: number) => () => {
+    const currentIndex = selectedNotifications.indexOf(value);
+    const newSelected = [...selectedNotifications];
 
-    const {
-      target: { value },
-    } = event;
-    const selected = typeof value === "string" ? value.split(",").map(Number) : value;
-
-    const addedNotifications = selected.filter((id) => !selectedNotifications.includes(id));
-    const removedNotifications = selectedNotifications.filter((id) => !selected.includes(id));
-
-
-    if (addedNotifications.length > 0) {
-      const request: any = {
-        monitorId: monitorId,
-        notificationId: addedNotifications[0],
-      };
-      MonitorService.addMonitorNotification(request).then(() => {
-      });
+    if (currentIndex === -1) {
+      newSelected.push(value);
+      const request = { monitorId: monitorId, notificationId: value };
+      MonitorService.addMonitorNotification(request).then(() => {});
+    } else {
+      newSelected.splice(currentIndex, 1);
+      const request = { monitorId: monitorId, notificationId: value };
+      MonitorService.removeMonitorNotification(request).then(() => {});
     }
 
-    if (removedNotifications.length > 0) {
-      const request: any = {
-        monitorId: monitorId,
-        notificationId: removedNotifications[0],
-      };
-      MonitorService.removeMonitorNotification(request).then(() => {
-      });
-    }
-
-    setSelectedNotifications(selected);
-
+    setSelectedNotifications(newSelected);
   };
-  useEffect(() => {
-    console.log(selectedNotifications);
-  }, [selectedNotifications]);
+
   return (
     <Dialog
       open={openDialog}
       onClose={handleCloseDialog}
       aria-labelledby="notification-dialog-title"
       aria-describedby="notification-dialog-description"
+      
     >
       <DialogTitle id="alert-dialog-title">
-        {t("notifications.title")}
+        {t('notifications.title')}
       </DialogTitle>
-      <FormControl sx={{ m: 1, width: 550 }}>
-        <InputLabel id="demo-multiple-checkbox-label">Notification</InputLabel>
-        <Select
-          labelId="demo-multiple-checkbox-label"
-          id="demo-multiple-checkbox"
-          multiple
-          value={selectedNotifications}
-          defaultValue={selectedNotifications}
-          onChange={handleChange}
-          input={<OutlinedInput label="Notification" />}
-          renderValue={(selected) => {
-            const valueMapping = allNotificationList.reduce(
-              (acc: any, item) => {
-                acc[item.id] = item.name;
-                return acc;
-              },
-              {}
+      <FormControl sx={{ m: 1, width: '500px' }}>
+        <List sx={{ width: '100%', maxWidth: '500ox' ,bgcolor: 'background.paper' }}>
+          {allNotificationList.map((item) => {
+            const labelId = `checkbox-list-label-${item.id}`;
+
+            return (
+              <ListItem
+                key={item.id}
+                disablePadding
+              >
+                <ListItemButton onClick={handleToggle(item.id)} dense>
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={selectedNotifications.indexOf(item.id) !== -1}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                  </ListItemIcon>
+                  <ListItemText id={labelId} primary={item.name} />
+                </ListItemButton>
+              </ListItem>
             );
-            return selected.map((id) => valueMapping[id] || id).join(", ");
-          }}
-        >
-          {allNotificationList.map((item) => (
-            <MenuItem key={item.id} value={item.id}>
-              <Checkbox checked={selectedNotifications.includes(item.id)} />
-              <ListItemText primary={item.name} />
-            </MenuItem>
-          ))}
-        </Select>
+          })}
+        </List>
       </FormControl>
     </Dialog>
   );
